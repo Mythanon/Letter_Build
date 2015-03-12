786786c0762d603683a769675538eec17a4d72ee
Mythanon <mythanon@gmail.com>
2015-03-12 16:57:47 -0400

    Initial VS2012 upload
    Baseline files, pretty basic, not really important at this stage of the
    company.

---
+++ Letter/Letter/drawing.cpp
@@ -0,0 +1,8 @@
+
+#include "SDL.h"
+#include "SDL_opengl.h"
+
+
+void Draw_Line(SDL_Renderer *Renderer, int X1, int Y1, int X2, int Y2)	{
+
+}
---
+++ Letter/Letter/main.cpp
@@ -0,0 +1,129 @@
+
+
+//DEFAULT INCLUDES
+
+
+#include "SDL.h"
+#include "SDL_opengl.h"
+//#include "SDL_image.h"
+#include <iostream>
+#include <string>
+
+SDL_Window *sdlWindow;
+SDL_Renderer *sdlRenderer;
+
+void draw_page()	{
+	SDL_SetRenderDrawColor(sdlRenderer, 0xFF, 0x00, 0xFF, 0xFF);
+	//SDL_RenderDrawPoint(sdlRenderer, 400, 300);
+	//SDL_RenderDrawPoint(sdlRenderer, 401, 300);
+	//SDL_RenderDrawPoint(sdlRenderer, 402, 300);
+	//SDL_RenderDrawPoint(sdlRenderer, 403, 300);
+	//SDL_RenderDrawPoint(sdlRenderer, 404, 300);
+}
+
+
+
+
+
+
+
+
+
+int main( int argc, char* argv[])	{
+	//initialize SDL
+	SDL_Init(SDL_INIT_EVERYTHING);
+  
+	//Set OpenGL memory usage
+	SDL_GL_SetAttribute( SDL_GL_RED_SIZE, 8 );
+	SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE, 8);
+	SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE, 8 );
+	SDL_GL_SetAttribute( SDL_GL_ALPHA_SIZE, 8);
+	SDL_GL_SetAttribute( SDL_GL_BUFFER_SIZE, 32);
+	SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE, 16 );
+	SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
+
+	//Init window
+	SDL_CreateWindowAndRenderer(0, 0, SDL_WINDOW_FULLSCREEN_DESKTOP, &sdlWindow, &sdlRenderer);
+
+
+	SDL_SetRenderDrawColor(sdlRenderer, 0, 0, 0, 255);
+	SDL_RenderClear(sdlRenderer);
+	//SDL_RenderCopy(sdlRenderer, sdlTexture, NULL, NULL); In the case we want to draw the screen using a texture (c# bitmap)
+
+
+
+
+	SDL_RenderPresent(sdlRenderer);
+
+	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");  // make the scaled rendering look smoother.
+	SDL_RenderSetLogicalSize(sdlRenderer, 1920, 1200);
+
+	SDL_SetRelativeMouseMode(SDL_TRUE);
+
+ 
+ 
+	std::cout << "OpenGL is running\n";
+	std::cout << "Main loop has started\n";
+
+	//Handles the main loop
+	bool isRunning = true;
+ 
+	//For handling with event
+	SDL_Event event;
+
+	while(isRunning == true)	{
+		 while ( SDL_PollEvent(&event) )	{
+			if (event.type == SDL_QUIT)	{
+				isRunning = false;
+			}
+			// SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR,
+			//             "Missing file",
+			//             "File is missing. Please reinstall the program.",
+			//            NULL);
+			
+
+
+			//Draw the page...
+
+
+
+
+			SDL_RenderClear(sdlRenderer);
+			SDL_SetRenderDrawColor(sdlRenderer, 0x00, 0x00, 0x00, 0xFF);
+			SDL_SetRenderDrawColor(sdlRenderer, 0xFF, 0x00, 0xFF, 0xFF);
+			SDL_RenderDrawPoint(sdlRenderer, 400, 300);
+			SDL_RenderDrawPoint(sdlRenderer, 401, 300);
+			SDL_RenderDrawPoint(sdlRenderer, 402, 300);
+			SDL_RenderDrawPoint(sdlRenderer, 403, 300);
+			SDL_RenderDrawPoint(sdlRenderer, 404, 300);
+			SDL_SetRenderDrawColor(sdlRenderer, 0x00, 0x00, 0x00, 0xFF);
+			SDL_RenderPresent(sdlRenderer);
+
+//			SDL_GL_SwapWindow(sdlWindow); //Swap to new frame
+		 }
+		SDL_Delay(1);
+	}
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
